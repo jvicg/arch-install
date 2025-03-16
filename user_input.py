cb4d5daa8779983091f9c4488ae10283e834c8ad
jcorreag <jcorreag@pm.me>
2025-03-16 22:10:48 +0100

    add: Add functionality to detect the valid disks
    Begin with the script to get the user input. At the moment it already
    obtain a list with valid disks. The list will contain an object Disk
    that will have some information about the disk.

--- user_input.py
+++ user_input.py
@@ -3,3 +3,10 @@
 
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
+
+import utils
+
+valid_disks = utils.get_valid_disks()
+
+for disk in valid_disks:
+    print(disk)
---
+++ utils/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .valid_disks import get_valid_disks
